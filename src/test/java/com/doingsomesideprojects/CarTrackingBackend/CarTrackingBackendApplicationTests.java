df96d5a722235ebd6b4320690ba8f4e908255fcc
Ben Loper <loperb06@gmail.com>
2023-09-29 17:30:42 -0400

    Add hello world endpoint

---
+++ src/test/java/com/doingsomesideprojects/CarTrackingBackend/CarTrackingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doingsomesideprojects.CarTrackingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTrackingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
